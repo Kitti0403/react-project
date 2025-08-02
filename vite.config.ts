660d07e0f1e63dde43611faf93595e414e4be1cf
Kitti0403 <annus.kitti@gmail.com>
2025-08-02 15:10:01 +0200

    Fix ESLint configuration and lint script

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/react-project/' : '/',
+  base: mode === 'production' ? '/react-project/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true
   }
-})
+}))
